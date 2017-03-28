50a6a6f286eac1470a380326c40d52dfb2038114
math@shaheen <liang.ma@studenti.polito.it>
2017-03-28 15:57:03 +0200

    add author information and license

--- distance_calc.cpp
+++ distance_calc.cpp
@@ -1,9 +1,40 @@
 #include "params.h"
 
+/* 
+======================================================
+ Copyright 2017 Liang Ma
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+======================================================
+======================================================
+ Copyright 2016 Fahad Bin Muslim
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+======================================================
+*
+* Author:   Liang Ma 
+*
+* This C++ code is based on the OpenCL code by Fahad Bin Muslim at https://github.com/fahadmuslim/KNN.
+* This kernel aims to computes the distance of all points in d_locations from the given
+* longitude and latitude, and returns it in the on-chip global buffer dist.
+* 
+*----------------------------------------------------------------------------
+*/
 
-// This computes the distance of all points in d_locations from the given
-// longitude and latitude, and returns it in the on-chip global buffer
-// dist.
 extern "C"
 void distance_calc(float2 *d_locations,
                      const float lat,
